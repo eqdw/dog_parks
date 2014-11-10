380e6da0555dc1cb55284c93637a19adbb13ac28
Tim Herd <rz@eqdw.net>
2014-11-09 21:22:35 -0800

    It's .NET NOT .COM

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ DogParks::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
 
-  config.action_mailer.default_url_options = { :host => 'actondogpark.com' }
+  config.action_mailer.default_url_options = { :host => 'actondogpark.net' }
   ActionMailer::Base.smtp_settings = {
     :address              => "smtp.sendgrid.net",
     :port                 => "587",
