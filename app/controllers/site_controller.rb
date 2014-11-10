f8557e80997e76327a5ab80f8ad958164ce64c09
Tim Herd <rz@eqdw.net>
2014-11-09 18:33:04 -0800

    controller backend

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,9 +1,17 @@
 class SiteController < ApplicationController
 
   def index
+    @recent_checkins = CheckIn.recent
   end
 
 
   def check_in
+    @check_in = CheckIn.new(:name => params[:name])
+
+    if @check_in.save
+
+    else
+
+    end
   end
 end
